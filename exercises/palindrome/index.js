8709dc25547f4b1be66176d70230cf72344da30b
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-26 10:00:38 -0400

    aplindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,30 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+ return  str.split('').every((char, i) => {
+    return char === str[str.length - i -1];
+  });
+}
 
 module.exports = palindrome;
+
+
+/*
+function palindrome(str) {
+    let reversed = str.split('').reverse().join('');
+     return reversed === str ? true : false;
+    }
+    
+    module.exports = palindrome;
+    */
+
+/*
+   function palindrome(str) {
+    const reversed = str.split('').reverse().join('');
+     return str === reversed;
+}
+
+module.exports = palindrome;
+
+*/
