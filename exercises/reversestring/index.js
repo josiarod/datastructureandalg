3972c5333d5ba692fb4a08c83c3f8bb5af1c4991
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-26 07:24:12 -0400

    Reverse a string.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,7 +5,33 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
-
-function reverse(str) {}
+/*
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
 
 module.exports = reverse;
+*/
+
+
+function reverse(str) {
+    let reversed = '';
+for (let character of str) {
+    reversed = character + reversed;
+    }
+return reversed; 
+}
+
+module.exports = reverse;
+
+
+
+//More advance
+/*
+function reverse(str) {
+   return  str.split('').reduce((rev, char) =>  char + rev ,'');
+}
+
+module.exports = reverse;
+
+*/
