501b9c332e9b295b7723a76fefced77a1942345a
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-28 09:04:18 -0400

    fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for(let i = 1; i <= n; i++){
+         //check if number is multiple of 3 and 5.
+         //you can also just check if it is multiple of 15. 
+       if(i % 3 === 0 && i % 5 === 0){
+     console.log('fizzbuzz');
+     //check if number is multiple of 3.
+        } else if(i % 3 === 0){
+            console.log('fizz');
+        }else if(i % 5 === 0){
+        console.log('buzz');
+        }else{
+            console.log(i);
+        }
+        
+    }
+}
 
 module.exports = fizzBuzz;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,16 +7,24 @@
 
 function maxChar(str) {
     let counter = {};
-
+    let max = 0;
+    let maxChar = '';
     for(let char of str){
         if(!counter[char]){
             counter[char] = 1;
         }else{
-            counter[char] += 1;
+            counter[char]++;
+        }
+        
+    }
+   
+    for (let char in counter){
+        if(counter[char] > max){
+            max = counter[char];
+        maxChar = char;
         }
     }
-
-   
+ return maxChar;
 }
 
 module.exports = maxChar;
