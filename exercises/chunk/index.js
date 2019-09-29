3ed467c8e046abce824a7aaddc550ba0675df06f
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-29 10:53:43 -0400

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while(index < array.length){
+      chunked.push(array.slice(index, index + size));
+      index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
+
+
+/*
+function chunk(array, size) {
+    const chunkedArray = [];
+    for (let i = 0; i < array.length; i++){
+        const last = chunkedArray[chunkedArray.length - 1];
+        if(!last || last.length === size){
+        chunkedArray.push([array[i]]);
+        }else {
+            last.push(array[i]);
+        }
+    }
+    return chunkedArray;
+}
+
+module.exports = chunk;
+
+*/
+
+/*
+function chunk(array, size) {
+    const chunkedArray = [];
+    for (let element of array){
+        const last = chunkedArray[chunkedArray.length - 1];
+        if(!last || last.length === size){
+        chunkedArray.push([element]);
+        }else {
+            last.push(element);
+        }
+    }
+    return chunkedArray;
+}
+
+module.exports = chunk;
+*/
