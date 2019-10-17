9d90f58f42972785ee51845aa4d41a7f2c20a725
Josia <josiarodriguez@gmail.com>
2019-10-17 16:24:37 -0400

    Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,42 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    if(row === n){
+        return;
+    }
+  if(level.length === n * 2 - 1){
+      console.log(level);
+      return pyramid(n,row + 1);
+  }
+
+  const midPoint = Math.floor((n * 2 - 1) / 2);
+  let add;
+  if(midPoint - row <= level.length && midPoint + row >= level.length) {
+      add = '#';
+  }else{
+      add = ' ';
+  }
+  pyramid(n, row, level + add);
+
+}
+
 
 module.exports = pyramid;
+/*
+function pyramid(n) {
+    const midPoint =  Math.floor((2 * n - 1)/2);
+    for(let row = 0; row < n; row++) {
+        let level = '';
+        for(let column = 0; column < 2 * n - 1 ; column++) {
+            if(midPoint - row <= column && midPoint + row >= column){
+                level += '#';
+            }else{
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+  
+  }
+  */
